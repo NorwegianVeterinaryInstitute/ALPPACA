0a5619580f9fe74bd4ec543cf6955ac17d506a04
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-09-01 10:56:45 +0200

    hard-coded script path

--- PhyloFlow.sh
+++ PhyloFlow.sh
@@ -2,13 +2,12 @@
 
 script_directory=$(dirname ${BASH_SOURCE[0]})
 
-script=$1
-config=$2
-outdir=$3
-workdir=${4:-$USERWORK/PhyloFlow_work}
+config=$1
+outdir=$2
+workdir=${3:-$USERWORK/PhyloFlow_work}
 
 mkdir -p ${outdir}/config_files
-cp ${script_directory}/${script} ${outdir}/config_files
+cp ${script_directory}/main.nf ${outdir}/config_files
 cp ${config} ${outdir}/config_files
 
-nextflow run ${script} -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
+nextflow run /cluster/projects/nn9305k/vi_src/PhyloFlow/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
