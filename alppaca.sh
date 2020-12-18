5d6c3ab716f468ef143e823448443b9f3c5ad629
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-12-18 12:57:20 +0100

    added version info output to config_files

--- alppaca.sh
+++ alppaca.sh
@@ -13,5 +13,8 @@ if [[ $type =~ "core" ]]; then workdir=${3:-$USERWORK/alppaca_core}; fi
 mkdir -p ${outdir}/config_files
 cp ${script_directory}/main.nf ${outdir}/config_files
 cp ${config} ${outdir}/config_files
+commitid=$(git --git-dir ${script_directory}/.git branch -v | grep "\*" | awk '{print $2, $3}')
+version=$(git --git-dir ${script_directory}/.git tag | tail -1)
+echo "Ellipsis version $version, commit-id $commitid" > ${outdir}/config_files/version.log
 
 nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
