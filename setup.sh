d978afda338e557e8176b71f399b4d763a686707
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2021-11-24 11:03:01 +0100

    changed to reporting completion for each download

--- setup.sh
+++ setup.sh
@@ -15,17 +15,21 @@ echo "Checking to see if all containers can be downloaded..."
 
 for url in $(cat container_paths) 
 do 
-  if curl --output /dev/null --silent --head --fail "$url"; then
-    echo "URL exists: $url"
-  else
-    echo "URL does not exist: $url"
-  fi 
+	if curl --output /dev/null --silent --head --fail "$url"; then
+		echo "URL exists: $url"
+	else
+		echo "URL does not exist: $url"
+		exit
+	fi 
 done
-
 
 #### Move to ouput directory and pull images
 mkdir ${container_dir}/alppaca_images
-cd ${container_dir}/alppaca_images
 
 echo "Starting download..."
-echo "All images downloaded!"
+for img in $(cat container_paths)
+do
+	img_name=${img##*/}
+	singularity pull ${container_dir}/alppaca_images/${img_name} $img
+	echo "$img_name complete"
+done
