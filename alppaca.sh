7cc61469c4a78feec96a3a38cfb8887695c156a1
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2021-11-24 11:02:07 +0100

    added additional reports, and changed the name of the three tracks

--- alppaca.sh
+++ alppaca.sh
@@ -5,10 +5,12 @@ script_directory=$(dirname ${BASH_SOURCE[0]})
 config=$1
 outdir=$2
 type=`grep "params.type" $config`
+container_dir='/cluster/projects/nn9305k/nextflow/alppaca_images'
+DATE=($(date "+%Y%m%d_%R"))
 
-if [[ $type =~ "assembly" ]]; then workdir=${3:-$USERWORK/alppaca_assembly}; fi
-if [[ $type =~ "reads" ]]; then workdir=${3:-$USERWORK/alppaca_reads}; fi
-if [[ $type =~ "core" ]]; then workdir=${3:-$USERWORK/alppaca_core}; fi
+if [[ $type =~ "core_genome" ]]; then workdir=${3:-$USERWORK/alppaca_core_genome}; fi
+if [[ $type =~ "mapping" ]]; then workdir=${3:-$USERWORK/alppaca_mapping}; fi
+if [[ $type =~ "core_gene" ]]; then workdir=${3:-$USERWORK/alppaca_core_gene}; fi
 
 mkdir -p ${outdir}/config_files
 cp ${script_directory}/main.nf ${outdir}/config_files
@@ -18,5 +20,5 @@ version=$(git --git-dir ${script_directory}/.git tag | tail -1)
 echo "ALPPACA version $version, commit-id $commitid" > ${outdir}/config_files/version.log
 
 module load Java/11.0.2
-nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
+nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} --container_dir=${container_dir} -work-dir ${workdir} -resume -with-report ${DATE}.report.html -with-trace
 module unload Java/11.0.2
