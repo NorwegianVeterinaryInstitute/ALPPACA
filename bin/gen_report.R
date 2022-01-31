1f62561706fa0ce6fc69258dc1b91e1de6892977
Håkon Kaspersen <hakon.kaspersen@protonmail.com>
2022-01-31 11:49:32 +0100

    added new scripts for generating reports for each workflow

---
+++ bin/gen_report.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+workflow <- args[1]
+
+# Generate rmarkdown report from ALPPACA run
+if (workflow == "core_genome") {
+  rmarkdown::render(
+    input  = 'bin/core_genome_report.Rmd',
+    params = list(
+      parsnp_report = "../testdata/PARSNP_results.txt",
+      phylo_data = "../testdata/IQTree_results.txt",
+      snpdist_report = "../testdata/SNPDIST_results.txt",
+      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
+      phylo_tree = "../testdata/IQTREE_tree.phylo"
+    )
+  )
+}
+
+if (workflow == "core_gene") {
+  rmarkdown::render(
+    input  = 'bin/core_gene_report.Rmd',
+    params = list(
+      ngenes_data = "../testdata/ngenes.txt",
+      ncontigs_data = "../testdata/ncontigs.txt",
+      pangenome_data = "../testdata/PANAROO_pangenome_results.txt",
+      mds_data = "../testdata/mds_coords.txt",
+      phylo_data = "../testdata/IQTree_results.txt",
+      snpdist_report = "../testdata/SNPDIST_results.txt",
+      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
+      phylo_tree = "../testdata/IQTREE_tree.phylo"
+    )
+  )
+}
+
+if (workflow == "mapping") {
+  rmarkdown::render(
+    input  = 'bin/mapping_report.Rmd',
+    params = list(
+      snippy_report = "../testdata/SNIPPY_results.txt",
+      phylo_data = "../testdata/IQTree_results.txt",
+      snpdist_report = "../testdata/SNPDIST_results.txt",
+      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
+      phylo_tree = "../testdata/IQTREE_tree.phylo"
+    )
+  )
+}
