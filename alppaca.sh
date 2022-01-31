87af61d320f4fa0f33b6f82f50d2c557f7d3b3d8
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2022-01-31 11:50:48 +0100

    fixed issue with work dir

--- alppaca.sh
+++ alppaca.sh
@@ -11,13 +11,11 @@ if [[ $type =~ core_genome ]]; then workdir=${work_path}/alppaca_core_genome; fi
 if [[ $type =~ mapping ]]; then workdir=${work_path}/alppaca_mapping; fi
 if [[ $type =~ core_gene ]]; then workdir=${work_path}/alppaca_core_gene; fi
 
-echo $workdir
+mkdir -p ${outdir}/config_files
+cp ${script_directory}/main.nf ${outdir}/config_files
+cp ${config} ${outdir}/config_files
+commitid=$(git --git-dir ${script_directory}/.git branch -v | grep "\*" | awk '{print $2, $3}')
+version=$(git --git-dir ${script_directory}/.git tag | tail -1)
+echo "ALPPACA version $version, commit-id $commitid" > ${outdir}/config_files/version.log
 
-#mkdir -p ${outdir}/config_files
-#cp ${script_directory}/main.nf ${outdir}/config_files
-#cp ${config} ${outdir}/config_files
-#commitid=$(git --git-dir ${script_directory}/.git branch -v | grep "\*" | awk '{print $2, $3}')
-#version=$(git --git-dir ${script_directory}/.git tag | tail -1)
-#echo "ALPPACA version $version, commit-id $commitid" > ${outdir}/config_files/version.log
-
-#nextflow_21.10.6 run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -profile singularity -resume -with-trace ${outdir}/results/NEXTFLOW_trace.txt -with-timeline ${outdir}/results/NEXTFLOW_timeline.html
+nextflow_21.10.6 run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -profile singularity -resume -with-trace ${outdir}/results/NEXTFLOW_trace.txt -with-timeline ${outdir}/results/NEXTFLOW_timeline.html
