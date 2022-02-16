cc8337d1a14f9e0e432ca87b2e39dcdbd04db6a9
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2022-02-16 12:27:06 +0100

    changed location of markdown script

--- bin/gen_report.R
+++ bin/gen_report.R
@@ -6,20 +6,20 @@ workflow <- args[1]
 # Generate rmarkdown report from ALPPACA run
 if (workflow == "core_genome") {
   rmarkdown::render(
-    input  = 'bin/core_genome_report.Rmd',
+    input  = 'core_genome_report.Rmd',
     params = list(
-      parsnp_report = "../testdata/PARSNP_results.txt",
-      phylo_data = "../testdata/IQTree_results.txt",
-      snpdist_report = "../testdata/SNPDIST_results.txt",
-      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
-      phylo_tree = "../testdata/IQTREE_tree.phylo"
+      parsnp_report = "parsnpAligner.log",
+      phylo_data = "iqtree.iqtree",
+      snpdist_report = "snp_dists.tab",
+      seqkit_report = "seqkit_list_duplicated",
+      phylo_tree = "iqtree.contree"
     )
   )
 }
 
 if (workflow == "core_gene") {
   rmarkdown::render(
-    input  = 'bin/core_gene_report.Rmd',
+    input  = 'core_gene_report.Rmd',
     params = list(
       ngenes_data = "../testdata/ngenes.txt",
       ncontigs_data = "../testdata/ncontigs.txt",
@@ -35,7 +35,7 @@ if (workflow == "core_gene") {
 
 if (workflow == "mapping") {
   rmarkdown::render(
-    input  = 'bin/mapping_report.Rmd',
+    input  = 'mapping_report.Rmd',
     params = list(
       snippy_report = "../testdata/SNIPPY_results.txt",
       phylo_data = "../testdata/IQTree_results.txt",
