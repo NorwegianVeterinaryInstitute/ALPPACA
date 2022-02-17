9e03ab6884b2aecb07c88845c7613d7c05cf5dcd
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2022-02-17 13:50:51 +0100

    added variable for deduplication detection

--- bin/gen_report.R
+++ bin/gen_report.R
@@ -2,6 +2,7 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 workflow <- args[1]
+dedup_val <- args[2]
 
 # Generate rmarkdown report from ALPPACA run
 if (workflow == "core_genome") {
@@ -12,7 +13,8 @@ if (workflow == "core_genome") {
       phylo_data = "iqtree.iqtree",
       snpdist_report = "snp_dists.tab",
       seqkit_report = "seqkit_list_duplicated",
-      phylo_tree = "iqtree.contree"
+      phylo_tree = "iqtree.contree",
+      dedup = dedup_val
     )
   )
 }
@@ -21,14 +23,15 @@ if (workflow == "core_gene") {
   rmarkdown::render(
     input  = 'core_gene_report.Rmd',
     params = list(
-      ngenes_data = "../testdata/ngenes.txt",
-      ncontigs_data = "../testdata/ncontigs.txt",
-      pangenome_data = "../testdata/PANAROO_pangenome_results.txt",
-      mds_data = "../testdata/mds_coords.txt",
-      phylo_data = "../testdata/IQTree_results.txt",
-      snpdist_report = "../testdata/SNPDIST_results.txt",
-      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
-      phylo_tree = "../testdata/IQTREE_tree.phylo"
+      ngenes_data = "ngenes.txt",
+      ncontigs_data = "ncontigs.txt",
+      pangenome_data = "summary_statistics.txt",
+      mds_data = "mds_coords.txt",
+      phylo_data = "iqtree.iqtree",
+      snpdist_report = "snp_dists.tab",
+      seqkit_report = "seqkit_list_duplicated",
+      phylo_tree = "iqtree.contree",
+      dedup_val = dedup
     )
   )
 }
@@ -37,11 +40,12 @@ if (workflow == "mapping") {
   rmarkdown::render(
     input  = 'mapping_report.Rmd',
     params = list(
-      snippy_report = "../testdata/SNIPPY_results.txt",
-      phylo_data = "../testdata/IQTree_results.txt",
-      snpdist_report = "../testdata/SNPDIST_results.txt",
-      seqkit_report = "../testdata/SEQKIT_duplicated_list.txt",
-      phylo_tree = "../testdata/IQTREE_tree.phylo"
+      snippy_report = "core.txt",
+      phylo_data = "iqtree.iqtree",
+      snpdist_report = "snp_dists.tab",
+      seqkit_report = "seqkit_list_duplicated",
+      phylo_tree = "iqtree.contree",
+      dedup = dedup_val
     )
   )
 }
